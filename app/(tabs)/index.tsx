fef37999b56f08d9a3cfe10d892c8c9055b18721
Wadah Esam <wadah-wleed@hotmail.com>
2025-09-17 16:25:37 +0300

    example

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { useEffect, useState } from 'react';
+import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
 
 export default function HomeScreen() {
+  const [direction, setDirection] = useState<'left' | 'right'>('left');
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -17,64 +17,182 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      <SomeAnimatedRow direction={direction} />
+      
     </ParallaxScrollView>
+  );
+}
+
+const SomeAnimatedRow = (direction = 'left') => {
+  
+  const rotate = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ rotate: `${rotate.value}deg` }],
+
+    };
+  });
+
+  useEffect(() => {
+    rotate.value = withRepeat(withTiming(direction === 'left' ? 25 : -25, { duration: Math.random() * 1000 }), -1, true);
+  }, [direction, rotate]);
+  
+  
+  return (
+    <View style={{ flexDirection: 'row', width: "100%", justifyContent: 'center', alignItems: 'center' }}>
+      <Animated.View style={[animatedStyle, {width: 100, height: 100, backgroundColor: Math.random() > 0.5 ? 'red' : 'blue' }]} />
+    </View>
   );
 }
 
