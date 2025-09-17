ebe9b83ad1181f5c05577e06195c31ca336247c4
Wadah Esam <wadah-wleed@hotmail.com>
2025-09-17 20:35:45 +0300

    enabling feature flags

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,5 @@
-import { Image } from 'expo-image';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
-import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { useEffect, useState } from 'react';
 import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
 
@@ -9,167 +7,12 @@ export default function HomeScreen() {
   const [direction, setDirection] = useState<'left' | 'right'>('left');
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
-      <SomeAnimatedRow direction={direction} />
+    <ScrollView contentContainerStyle={{ gap: 10 }}>
+      {Array.from({ length: 300 }).map((_, index) => (
+        <SomeAnimatedRow key={index} direction={direction} />
+      ))}
       
-    </ParallaxScrollView>
+    </ScrollView>
   );
 }
 
